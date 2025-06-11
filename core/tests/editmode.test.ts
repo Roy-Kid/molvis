e767b8fcec7bcdfdc2aae8fd56db2a9a4930bed4
Jichen Li <42854324+Roy-Kid@users.noreply.github.com>
2025-06-11 16:11:53 +0200

    Implement edit mode menu (#5)
    * Add edit mode menu with periodic table plugin
    
    * Show menu only when no mesh is picked
    
    * Fix periodic table plugin registration and set menu title
    
    * Fix tweakpane plugin registration
    
    * fix plugin compatibility
    
    * Remove periodic table plugin and use list
    
    * Implement multi-order bonds
    
    * Fix multi-bond offsets and click behavior

--- core/tests/editmode.test.ts
+++ core/tests/editmode.test.ts
@@ -58,9 +58,13 @@ describe('EditMode', () => {
     const mode = new EditMode(app);
     mode._on_pointer_down({ event: { button: 0, clientX: 0, clientY: 0 } } as any);
 <<<<<<< HEAD
+<<<<<<< HEAD
     mode._on_pointer_up({ event: { button: 0, clientX: 0, clientY: 0 } } as any);
 =======
 >>>>>>> 6aa5119 ([feat] add edite mode)
+=======
+    mode._on_pointer_up({ event: { button: 0, clientX: 0, clientY: 0 } } as any);
+>>>>>>> d35dc9b (Implement edit mode menu (#5))
     expect(system.current_frame.atoms.length).toBe(1);
     mode.finish();
   });
